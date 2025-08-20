70c3e7576f716a6665c502dff3d0878da0327e2c
devzwide <nxumalobukeka4@gmail.com>
2025-08-20 09:56:46 +0200

    feat: add CORS support to the Express server and update dependencies

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 import router from "./routes/router.js";
 
 const server = express();
 
 server.use(express.json());
+server.use(cors({
+    origin: process.env.CORS_ORIGIN,
+}));
 
 server.use("/api", router);
 
